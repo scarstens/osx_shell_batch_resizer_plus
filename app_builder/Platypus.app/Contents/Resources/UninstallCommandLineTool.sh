cbe8a329cc3bcb32889eb01a4adad17151eccb26
Seth Carstens <seth@sethmatics.com>
2015-10-30 18:10:20 -0700

    added new app resources

---
+++ app_builder/Platypus.app/Contents/Resources/UninstallCommandLineTool.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# UninstallCommandLineTool.sh
+# Platypus
+#
+# Created by Sveinbjorn Thordarson on 6/17/08.
+
+echo "Uninstalling command line tool" > /dev/stderr
+
+# Delete resources
+if [ -e "/usr/local/share/platypus/" ]
+then
+    echo "Deleting /usr/local/share/platypus/ directory" > /dev/stderr
+    rm -R "/usr/local/share/platypus/"
+fi
+
+if [ -e "/usr/local/bin/platypus" ]
+then
+    echo "Deleting platypus command line tool in /usr/local/bin/platypus" > /dev/stderr
+    rm "/usr/local/bin/platypus"
+fi
+
+if [ -e "/usr/local/share/man/man1/platypus.1" ]
+then
+    echo "Deleting platypus man page" > /dev/stderr
+    rm "/usr/local/share/man/man1/platypus.1"
+fi
